1c93a0a27f7178b4ff81e92b9e96be3ba6ad0822
Michael Kamau <tokamau@gmail.com>
2016-10-09 20:04:52 +0300

    Update function doc_string

--- data_types_lab/data_type.py
+++ data_types_lab/data_type.py
@@ -1,5 +1,18 @@
-
 def data_type(value):
+    """
+    This function does comparisons on the parameter and returns an output according to the type of the argument
+
+    The following rules are used:
+        - For strings, return its length.
+        - For None return string 'no value'
+        - For booleans return the boolean passed
+        - For integers return a string showing how it compares to 100
+        - For lists return the 3rd item, or None if it doesn't exist
+
+    :param: value
+
+    :return: outcome depending on the type of the argument passed
+    """
     if isinstance(value, str):
         return len(value)
     elif isinstance(value, type(None)):
