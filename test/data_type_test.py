40ea75f74e16f04cb67b8b2bc0d937ce13a74a70
Michael Kamau <tokamau@gmail.com>
2016-10-09 19:25:02 +0300

    Add test for None args

--- test/data_type_test.py
+++ test/data_type_test.py
@@ -3,8 +3,14 @@ from data_types_lab.data_type import data_type
 
 
 class DataTypeComparisons(unittest.TestCase):
-    def test_string_type(self):
+    def test_string_type_arg(self):
         """
         For argument of string type, data_type should return the length of the string
         """
         self.assertEqual(3, data_type('abc'))
+
+    def test_none_type_arg(self):
+        """
+        For None args, data_type should return a string 'no_value'
+        """
+        self.assertEqual('no value', data_type(None))
